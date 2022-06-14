59ef63408624e87d73da7fde60aaa157bddeadc7
JuanGalarza <groupfuzion@gmail.com>
2022-06-14 11:16:42 -0300

    Add contact.js - Edit Index & global.css

---
+++ components/contact.js
@@ -0,0 +1,75 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import NoSsr from "@mui/base/NoSsr";
+
+export default function Contact() {
+  return (
+    <NoSsr>
+      <Box
+        sx={{
+          height: "100vh",
+          padding: "0 24px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item xs={12} xl={6}>
+            <h1>Map</h1>
+          </Grid>
+          <Grid item xs={12} xl={6}>
+            <Grid
+              container
+              direction="row"
+              justifyContent="start-flex"
+              alignItems="center"
+              spacing={2}
+            >
+              <h1>Permanezcamos en contacto!</h1>
+              <Grid item xs={12} xl={8}>
+                <TextField
+                  fullWidth
+                  id="outlined-textarea"
+                  label="Nombre y Apellido"
+                  placeholder="Nombre y Apellido"
+                  multiline
+                />
+              </Grid>
+              <Grid item xs={12} xl={8}>
+                <TextField
+                  fullWidth
+                  id="outlined-textarea"
+                  label="Email"
+                  placeholder="Email"
+                  multiline
+                />
+              </Grid>
+              <Grid item xs={12} xl={8}>
+                <TextField
+                  fullWidth
+                  id="outlined-multiline-static"
+                  label="Mensaje"
+                  placeholder="Dejenos su mensaje"
+                  multiline
+                  rows={4}
+                />
+              </Grid>
+                <Grid item xs={12} xl={8}>
+                <Button variant="outlined">Enviar mensaje</Button>
+                </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </NoSsr>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,30 @@
-import * as React from 'react';
-import Navbar from '../components/navbar';
-import Hero from '../components/hero';
-import Cards from '../components/cards';
-import Button from '@mui/material/Button';
-import Masonry from '../components/masonry';
-import Footer from '../components/footer';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import Cards from "../components/cards";
+import Masonry from "../components/masonry";
+import Contacto from "../components/contact";
+import Footer from "../components/footer";
 
 const Home = () => (
   <div>
-    <header>
-      <Navbar />
-    </header>
-    <main>
-      <section className='hero'>
-        <Hero />
-      </section>
-      <section className='sec-cards'>
-        <h1 className='title'>Nuestros productos</h1>
-        <Cards variant='outlined' />
-        <div className='btn-center'>
-          <Button className='btn-cards'>
-            Ver mas productos
-          </Button>
-        </div>
-        
-      </section>
-      <Masonry />
-      <section className='sec-footer'>
-        <Footer />
-      </section>
-    </main>
+    
+    <Navbar />
+
+    <Hero />
+
+    <h1 className="title">Nuestros productos</h1>
+    <Cards variant="outlined" />
+    <div className="btn-center">
+      <Button variant="outlined">Ver mas productos</Button>
+    </div>
+
+    <Masonry />
+
+    <Contacto />
+
+    <Footer />
   </div>
 );
 
