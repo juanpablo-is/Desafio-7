b4787663ff550c8fdbd8d8ad4aa909f0698b309f
Juan Galarza <98394325+juangalarza@users.noreply.github.com>
2022-06-15 15:00:26 -0300

    Update contact.js

--- components/contact.js
+++ components/contact.js
@@ -35,7 +35,7 @@ export default function Contact() {
               spacing={2}
             >
               <h1>Permanezcamos en contacto!</h1>
-              <Grid item xs={12} md={6} xl={8}>
+              <Grid item xs={12} md={6} xl={6}>
                 <TextField
                   fullWidth
                   id="outlined-textarea"
